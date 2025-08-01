b4673ecd5130223aaff684ca961c145bf030e1ea
rena-biaobock <rena.biaobock.costa@gmail.com>
2025-07-31 21:16:45 -0300

    :sparkles: feat: variaveis

---
+++ javascript-basico/03-variaveis/index.js
@@ -0,0 +1,5 @@
+var variavelGlobal  = "variavel mutavel de escopo global";
+
+const variavelConstante = "variavel constante, nao pode ser alterada após criada";
+
+let variavelEscopoLocal = "variavel mutavel de escopo local";
