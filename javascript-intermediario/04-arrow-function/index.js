fc831b92a608105a1a7e246fbc2343f2cc036cfd
rena-biaobock <rena.biaobock.costa@gmail.com>
2025-08-04 23:02:52 -0300

    :sparkles: feat: arrow function

---
+++ javascript-intermediario/04-arrow-function/index.js
@@ -0,0 +1,10 @@
+let sayHello = name => {
+    console.log('Hello ' + name + '!')
+}
+
+sayHello('Name')
+
+
+let resultadoDaSoma = (numero1, numero2) => numero1 + numero2
+
+console.log(resultadoDaSoma(4, 7))
