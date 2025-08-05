2ddb5fc8504a4b90c72a851e95ac70a3dc78a627
rena-biaobock <rena.biaobock.costa@gmail.com>
2025-08-04 22:42:10 -0300

    :sparkles: feat: escopo no JS

---
+++ javascript-intermediario/01-escopo-no-javascript/index.js
@@ -0,0 +1,20 @@
+const mensagem = 'Olá'
+
+console.log(mensagem)
+
+if (true) {
+    var mensagem2 = 'Olá'
+}
+
+console.log(mensagem2)
+
+{
+    let mensagem3 = 'Olá'
+}
+
+// console.log(mensagem3)
+
+function ola() {
+    var mensagem4 = 'Olá'
+    console.log(mensagem4)
+}
