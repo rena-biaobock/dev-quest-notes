079a63b20393ea1797fa3a74fc56d563c53e344d
rena-biaobock <rena.biaobock.costa@gmail.com>
2025-08-16 10:12:14 -0300

    :sparkles: feat: interpolaçã de strings

---
+++ javascript-avancado/04-interpolacao-de-string/index.js
@@ -0,0 +1,4 @@
+ const imprimirInformacoesPessoa = (primeiroNome, ultimoNome, idade) => `Primeiro Nome: ${primeiroNome}, Último Nome: ${ultimoNome}, Idade: ${idade}`
+ console.log(`${imprimirInformacoesPessoa('Roberto', 'Dias', 33)}, é uma pessoa.`)
+
+ console.log(`Soma: ${10 + 20}`)
