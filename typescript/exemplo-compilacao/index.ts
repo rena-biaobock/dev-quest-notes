c7a26d1e7a062cab5adc1ec25a4ea36e6977c84c
rena-biaobock <rena.biaobock.costa@gmail.com>
2025-08-24 10:57:39 -0300

    :sparkles: feat: TS compiler example

---
+++ typescript/exemplo-compilacao/index.ts
@@ -0,0 +1,6 @@
+function sendMessage(userName: string, isLogged: boolean) {
+   console.log(`Olá ${userName}. Você ${!isLogged? " não" : ""} pode logar.`)
+}
+const userName = "Beto"
+const isLogged = true
+sendMessage(userName, isLogged)
