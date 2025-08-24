a89b42c88383ed1f1a0dce3d43d14159af30df41
rena-biaobock <rena.biaobock.costa@gmail.com>
2025-08-24 11:56:28 -0300

    :sparkles: feat: TS enums

---
+++ typescript/enums/index.ts
@@ -0,0 +1,18 @@
+enum Colors {
+    Red,
+    Blue,
+    Green
+}
+
+function showColor(color) {
+    console.log(color)
+}
+
+showColor(Colors.Red)
+
+enum TradeType {
+    ACAO = 9281,
+    TESOURO_SELIC = 6554
+}
+ 
+TradeType.ACAO
