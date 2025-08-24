f284c1dc5d270f19bd0a509cf5c0f3fd009dfa1c
rena-biaobock <rena.biaobock.costa@gmail.com>
2025-08-24 13:22:35 -0300

    :sparkles: feat: TS object typing

---
+++ typescript/tipagem-em-objetos/index.ts
@@ -0,0 +1,9 @@
+type Customer = {
+  name: string;
+  age: number;
+};
+
+function showCustomerInfo(customer: Customer) {
+  console.log(`O nome do cliente é: ${customer.name}`);
+  console.log(`A idade do cliente é: ${customer.age}`);
+}
