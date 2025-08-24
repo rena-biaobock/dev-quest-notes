96a752f12db603bf8bf7b340e6848742cddd5a9b
rena-biaobock <rena.biaobock.costa@gmail.com>
2025-08-24 11:19:52 -0300

    :sparkles: feat: TS types

---
+++ typescript/entenda-os-tipos/index.ts
@@ -0,0 +1,15 @@
+let nome: string = "Beto";
+let idade: number = 33;
+let mandouEmail: boolean = true;
+
+let idades: number[] = [12, 22, 39];
+
+let any: any = { x:'y' }
+
+let obj: {
+    x: number;
+    y: number
+} = {
+    x: 3,
+    y: 2
+};
