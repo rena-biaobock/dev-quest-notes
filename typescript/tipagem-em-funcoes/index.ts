4ee65f58b3ed97e1684aa00474f5b03101443b63
rena-biaobock <rena.biaobock.costa@gmail.com>
2025-08-24 13:08:25 -0300

    :sparkles: feat: TS function typing

---
+++ typescript/tipagem-em-funcoes/index.ts
@@ -0,0 +1,12 @@
+function greet(name: string) {
+  console.log(`Olá, ${name}!`);
+}
+
+greet("Ricardo");
+
+function doubleNumber(number: number): number {
+  return number * 2;
+}
+
+const result = doubleNumber(3);
+console.log(result);
