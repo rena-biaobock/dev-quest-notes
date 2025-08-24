609518cc811f34a0566cb989faf549cbeb35e9d7
rena-biaobock <rena.biaobock.costa@gmail.com>
2025-08-24 11:40:20 -0300

    :sparkles: feat: TS tuples

---
+++ typescript/tuplas/index.ts
@@ -0,0 +1,5 @@
+const stringNumber: [string, number] = ["Roberto", 123]
+
+const [name, setName] = useState("Beto");
+let RGB: [number, number, number] = [255, 0, 0];
+let bgColor: [number, number, number, number?] = [255, 0, 0, 0.5];
